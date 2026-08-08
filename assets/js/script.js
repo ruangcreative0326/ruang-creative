a5a8aaf73bd0412789c1a1f8efc002333639b8b2
ruangcreative0326 <ruangcreative0326@gmail.com>
2026-08-08 11:41:10 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,37 @@
+// ==========================
+// FAQ ACCORDION
+// ==========================
+
+const faqQuestions = document.querySelectorAll(".faq-question");
+
+
+faqQuestions.forEach(question => {
+
+
+    question.addEventListener("click", () => {
+
+
+        const answer = question.nextElementSibling;
+        const icon = question.querySelector("span");
+
+
+        answer.classList.toggle("active");
+
+
+        if(answer.classList.contains("active")){
+
+            icon.textContent = "−";
+
+        } else {
+
+            icon.textContent = "+";
+
+        }
+
+
+    });
+
+
+});
+
 
