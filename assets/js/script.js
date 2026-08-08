66619c0be71862c4bdfba298d32e25099ff18b87
ruangcreative0326 <ruangcreative0326@gmail.com>
2026-08-08 19:49:11 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,4 +34,115 @@ faqQuestions.forEach(question => {
 
 });
 
+// ==========================
+// GALLERY LIGHTBOX
+// ==========================
 
+const galleryImages = document.querySelectorAll(".gallery-img");
+
+const lightbox = document.getElementById("lightbox");
+
+const lightboxImage = lightbox.querySelector("img");
+
+const lightboxClose = document.querySelector(".lightbox-close");
+
+
+
+galleryImages.forEach(image => {
+
+
+    image.addEventListener("click", () => {
+
+
+        lightbox.classList.add("active");
+
+
+        lightboxImage.src = image.src;
+
+
+    });
+
+
+});
+
+
+
+lightboxClose.addEventListener("click", () => {
+
+
+    lightbox.classList.remove("active");
+
+
+});
+
+
+
+lightbox.addEventListener("click", (e) => {
+
+
+    if(e.target === lightbox){
+
+        lightbox.classList.remove("active");
+
+    }
+
+
+});
+// ==========================
+// BOOKING WHATSAPP FORM
+// ==========================
+
+
+const bookingBtn = document.getElementById("bookingBtn");
+
+
+if(bookingBtn){
+
+
+bookingBtn.addEventListener("click", function(){
+
+
+    const nama = document.getElementById("nama").value;
+
+    const nomor = document.getElementById("nomor").value;
+
+    const paket = document.getElementById("paket").value;
+
+    const tanggal = document.getElementById("tanggal").value;
+
+    const pesan = document.getElementById("pesan").value;
+
+
+
+    const text =
+
+`Halo Ruang Creative,
+
+Saya ingin melakukan booking.
+
+Nama: ${nama}
+
+Nomor WhatsApp: ${nomor}
+
+Paket: ${paket}
+
+Tanggal Foto: ${tanggal}
+
+Catatan:
+${pesan}`;
+
+
+
+    const url =
+
+`https://wa.me/6282134190135?text=${encodeURIComponent(text)}`;
+
+
+
+    window.open(url, "_blank");
+
+
+});
+
+
+}
